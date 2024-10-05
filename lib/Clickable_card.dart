24e9f7349d1f8579a9f4bd3cf19ac858f809e995
HeshmatGhaffariHadid <https://github.com/HeshmatGhaffariHadid>
2024-10-05 23:36:22 +0430

    Details from all masjids added

--- lib/Clickable_card.dart
+++ lib/Clickable_card.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
-class ClickableCard extends StatelessWidget {
+class ClickableCard extends StatefulWidget {
   final String title;
 ClickableCard({required this.title });
+
+  @override
+  State<ClickableCard> createState() => _ClickableCardState();
+}
+
+class _ClickableCardState extends State<ClickableCard> {
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -9,21 +15,25 @@ ClickableCard({required this.title });
         padding: const EdgeInsets.all(4),
         child: GestureDetector(
           onTap: () {
-            print(title);
+            print(widget.title);
           },
           child: Container(
-            width: double.infinity,
             decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(10),
+              // color: Colors.blue,
+              gradient: LinearGradient(
+                  colors: [Colors.blue.shade900, Colors.blueAccent.shade400],
+              ),
+              borderRadius: BorderRadius.circular(15),
               border: Border.all(color: Colors.blue.shade800),
             ),
             child: Center(
-              child: Text(title,
+              child: Text(widget.title,
                   style: TextStyle(
                       color: Colors.white,
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold)),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                  ),
+              ),
             ),
           ),
         ),
---
+++ lib/datails.dart
@@ -0,0 +1,14 @@
+class Details {
+   final Map<String, String> info = {
+    'Masjid al-Haram' : 'Masjid al-Haram is the Muslim\'s Qeblah which is located in Mecca, Saudi Arabia, center of earth, this great and holy masjid was build by prophet Ibrahim <PUH> and this masjid is the holiest masjid in Islam that surround the Kaaba, and is the focal point of Hajj pilgrimage',
+     'Masjid an-Nabawi':'Masji an-Nabawi is prophet\'s masjid which is located in Madina, Saudi Arabia, second holiest masjid in Islam, houses the tomb of Prophet Muhammad <PUH> and is a revered site for Muslims',
+       'Al-Aqsa Masjid' : 'Al-Aqsa masjid is located in Jerusalem, Palestine, third holiest masjid in Islam which was the first Qableh in Islam and the Meraj accident was happened their, unfortunately it\'s now under control of Jewish people, so this is our responsibility to take it back',
+   'Sheikh Zayed Grand Masjid' : 'Sheikh Zayed Grand masjid is located in Abu Dhabi,UAE, it is the largest masjid in UAE, known for it\;s beautiful architecture, including domes, minarets, and intricate design',
+     'Sultan Ahmad masjid': 'Sultan Ahmad masjid is popular as Blue Masjid in Istanbul, Turkey, it is a iconic masjid with stunning blue tiles, six minarets, and intricate calligraphy, blending Islam and Ottoman architecture',
+'Hassan II masjid' : 'Hassan II masjid is located in Casablanca, Morocco, one of the largest masjids in the world, overlooking the Atlantic Ocean, featuring intricate mosaics and a towering minaret',
+     'Sheikh Lotfollah masjid' : 'Sheikh Lotfollah masjid is located in Isfahan, Iran, masterpiece od Persian architecture, known for it\'s stunning dome, intricate tile work, and beautiful iwans',
+     'Badshahi Masjid' : 'Badshahi masjid in located in Lahore, Pakistan, one of the largest masjid from the Mughal era, known for it\'s grandeur and historical significance',
+     'Quba Masjid' : 'Quba masjid located in Madina city of Saudi Arabia, this is the oldest masjid in the Islam which was built by Prophet Muhammad <PUH>, and holds spiritual significance for muslims ',
+     'Faisal Masjid': 'Faisal masjid is located in Islamabad Pakistan, known for it\'s unique contemporary design inspired by Bedouin tents'
+   };
+}
